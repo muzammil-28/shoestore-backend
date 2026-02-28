01fc988f5f8c1336fac9dc45cb535d7516fb038c
Muzammil Islam <muzammilislam31@gmail.com>
2026-02-28 19:53:39 +0500

    navbar code update

--- config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db_host = "localhost";
-    $db_username = "root";
-    $db_password = "";
-    $db_name = "shoe-store";
-
-    $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
-?>
---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+    $db_host = "sql302.infinityfree.com";
+    $db_username = "if0_41263318";
+    $db_password = "Muzzi123muz";
+    $db_name = "shoestore";
+
+    $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+?>
