ce4057b3654fa08a2507de6cbdad21e686d25235
Muzammil Islam <muzammilislam31@gmail.com>
2026-03-01 14:37:55 +0500

    database info change

--- config/database.php
+++ config/database.php
@@ -1,9 +1,8 @@
 <?php
-    $db_host = "mysql.railway.internal";
-    $db_username = "root";
-    $db_password = "FPDNNNMtUIPUOFWzBUaJDFneQtWJPAhe";
-    $db_name = "railway";
-    $db_port = "3306";
+    $db_host = "fdb1032.awardspace.net";
+    $db_username = "4739337_shoestore";
+    $db_password = "Muzzi@12345";
+    $db_name = "4739337_shoestore";
 
-    $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name, $db_port);
+    $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 ?>
