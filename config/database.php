f0d5a112ca7cd7d704e54bef12d96c84400f3990
Muzammil Islam <muzammilislam31@gmail.com>
2026-03-01 13:08:41 +0500

    database file update

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+    $db_host = "mysql.railway.internal";
+    $db_username = "root";
+    $db_password = "FPDNNNMtUIPUOFWzBUaJDFneQtWJPAhe";
+    $db_name = "railway";
+    $db_port = "3306";
+
+    $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name, $db_port);
+?>
