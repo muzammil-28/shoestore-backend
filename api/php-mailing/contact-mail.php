3b0d81ca26b9067a850d8f606969032747af2a65
Muzammil Islam <muzammilislam31@gmail.com>
2026-03-04 15:52:40 +0500

    contact form code update

--- api/php-mailing/contact-mail.php
+++ api/php-mailing/contact-mail.php
@@ -9,8 +9,8 @@
 
     header("Content-Type: application/json");
     header("Access-Control-Allow-Origin: http://localhost:5173");
-    header("Access-Control-Allow-Methods: POST, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorize");
 
     if($_SERVER["REQUEST_METHOD"] === "OPTIONS")
     {
